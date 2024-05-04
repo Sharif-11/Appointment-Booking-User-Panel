78c3d7c4b53774fa416d6a61b3fb008a2dfdc286
Sharif-11 <sharifsharifulislam061@gmail.com>
2024-05-04 20:03:44 +0600

    Loading effect added with appointment route

--- src/Components/Appointments.tsx
+++ src/Components/Appointments.tsx
@@ -1,13 +1,21 @@
 import { useEffect, useState } from "react";
+import { HashLoader } from "react-spinners";
 import axiosInstance from "../Axios/axios";
 import Appointment from "./Appointment";
 const Appointments = () => {
   const [appointments, setAppointments] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     axiosInstance
       .get("/appointments")
-      .then(({ data }) => data.status && setAppointments(data.data))
-      .catch(() => setAppointments([]));
+      .then(({ data }) => {
+        data.status && setAppointments(data.data);
+        setLoading(false);
+      })
+      .catch(() => {
+        setAppointments([]);
+        setLoading(false);
+      });
   }, []);
   // useEffect(() => {
   //   const eventSource = new EventSource(
@@ -26,16 +34,20 @@ const Appointments = () => {
   return (
     <div className="m-12 py-3">
       <h1 className=" text-center my-[48px]">Upcoming Appointments</h1>
-
-      <div className="w-full my-auto flex flex-wrap p-8">
-        {appointments.map((appointment, idx) => (
-          <Appointment key={idx} {...appointment} />
-        ))}
-      </div>
-      {appointments.length === 0 && (
+      {loading ? (
+        <div className="flex justify-center items-center">
+          <HashLoader size={125} />
+        </div>
+      ) : appointments.length === 0 ? (
         <h1 className="text-center text-[gray] my-auto text-[16px]">
           There is no upcoming Appointments
         </h1>
+      ) : (
+        <div className="w-full my-auto flex flex-wrap p-8">
+          {appointments.map((appointment, idx) => (
+            <Appointment key={idx} {...appointment} />
+          ))}
+        </div>
       )}
     </div>
   );
