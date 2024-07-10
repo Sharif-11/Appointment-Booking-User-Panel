f65f91bc7f82f9ef4f9f406f0b43736cb7c8367d
Sharif-11 <sharifsharifulislam061@gmail.com>
2024-07-10 19:20:30 +0600

    tsconfig edited

--- src/Axios/axios.ts
+++ src/Axios/axios.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const baseURL = "http://localhost:5000/api/v1/";
+export const baseURL =
+  "https://appointment-booking-server-3.onrender.com/api/v1/";
 const axiosInstance = axios.create({ baseURL, withCredentials: true });
 export const setHeader = (token: string) => {
   axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
